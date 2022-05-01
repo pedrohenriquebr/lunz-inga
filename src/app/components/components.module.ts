851bf96666abc606fcab60f9080cb6edbd18b1c3
pedrohenriquebr <pedrohenriquebraga735@gmail>
2022-05-01 01:07:07 -0300

    Add Angular files #1

---
+++ src/app/components/components.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NewsletterComponent } from './newsletter/newsletter.component';
+
+
+
+@NgModule({
+  declarations: [
+    NewsletterComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    NewsletterComponent
+  ]
+})
+export class ComponentsModule { }
