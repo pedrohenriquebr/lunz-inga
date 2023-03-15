e66747453fb271536494f4e6e314593ee91fa00e
Pedro Henrique Braga da Silva <pedrohenriquebraga735@gmail.com>
2023-03-15 12:03:07 -0300

    refactor: using bloom filter for check if exists email

--- backend/LuzInga.Application/Usecases/Contact/CheckEmail/CheckEmailWithBloomFilter.Handler.cs
+++ backend/LuzInga.Application/Usecases/Contact/CheckEmail/CheckEmailWithBloomFilter.Handler.cs
@@ -1,27 +1,26 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Ardalis.ApiEndpoints;
+using LuzInga.Application.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Swashbuckle.AspNetCore.Annotations;
 
 namespace LuzInga.Application.Usecases.Contact.CheckEmail;
 
-public class CheckEmailHandler
+public class CheckEmailWithBoomFilterHandler
     : EndpointBaseAsync.WithoutRequest.WithActionResult<CheckEmailResponse>
 {
     private readonly IDbContext context;
+    private readonly IBloomFilter bloomFilter;
 
-    public CheckEmailHandler(IDbContext context)
+    public CheckEmailWithBoomFilterHandler(IDbContext context, IBloomFilter filter)
     {
         this.context = context;
+        this.bloomFilter = filter;
     }
 
-    [HttpGet("/api/contact/check-email/{email}")]
+    [HttpGet("/api/contact/check-email/v2/{email}")]
     [SwaggerOperation(
-        Summary = "Check if emails exists in the contacts table on database",
+        Summary = "Check if emails exists in bloom filter first, then checks on the contacts table on database",
         Description = "Return if exists email",
         OperationId = "Contact.Checkemail",
         Tags = new[] { "Contact" }
@@ -33,10 +32,16 @@ public class CheckEmailHandler
         //todo: validate email format with regex
         var emailResquested = RouteData.Values["email"].ToString();
 
-        var existsEmail = await context.Contact.AnyAsync(
-            x => x.Email == emailResquested,
-            cancellationToken
-        );
-        return Ok(new CheckEmailResponse(existsEmail));
+        if (bloomFilter.MaybeContains(emailResquested!))
+            return Ok(
+                new CheckEmailResponse(
+                    await context.Contact.AnyAsync(
+                        x => x.Email == emailResquested,
+                        cancellationToken
+                    )
+                )
+            );
+
+        return Ok(new CheckEmailResponse(false));
     }
 }
