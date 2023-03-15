2311b7d53cf6fa25f16e2dbfd29e545365d3a361
Pedro Henrique Braga da Silva <pedrohenriquebraga735@gmail.com>
2023-03-15 12:02:36 -0300

    refactor: depedency injection for BloomFilter
    TODO: create env vars for bloom filter options

--- backend/LuzInga.Application/DependencyInjection.cs
+++ backend/LuzInga.Application/DependencyInjection.cs
@@ -1,13 +1,36 @@
+using LuzInga.Application.Services;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using System.Linq;
 
 namespace LuzInga.Application;
 
 public static class DependencyInjection
 {
-    
-    public static WebApplicationBuilder AddApplication(this WebApplicationBuilder  builder)
+    public static WebApplicationBuilder AddApplication(this WebApplicationBuilder builder)
     {
+        builder.Services.AddBloomFilter();
 
         return builder;
+    }
+
+    public static IServiceCollection AddBloomFilter(this IServiceCollection collection)
+    {
+        using var scope = collection.BuildServiceProvider().CreateScope();
+        var context = scope.ServiceProvider.GetRequiredService<IDbContext>();
+        var contactsList = context.Contact.ToList().Select(contact => contact.Email).ToList();
+
+        collection.AddSingleton<IBloomFilter>(
+            provider =>
+                DefaultBloomFilter.CreateFrom(
+                    contactsList,
+                    options =>
+                    {
+                        options.MinSize = 1_000;
+                    }
+                )
+        );
+
+        return collection;
     }
 }
